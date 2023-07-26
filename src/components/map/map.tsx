73e4bd94253f480a2b63a63d2601e4832cf31b78
Pronin Maks <maksimpronin1991@icloud.com>
2023-07-26 18:59:50 +0300

    beter

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef,MutableRefObject} from 'react';
 import 'leaflet/dist/leaflet.css';
 import leaflet from 'leaflet';
+import layerGroup from 'leaflet';
 import useMap from '../custom-hooks/use-map';
 import { URL_MARKER_CURRENT, URL_MARKER_DEFAULT } from '../../consts';
 import { City, Offers } from '../../types/offer';
@@ -10,32 +11,34 @@ type MapScreenProps = {
   points: Offers;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const defaultCustomIcon = leaflet.icon({
+  iconUrl: URL_MARKER_DEFAULT,
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+});
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const currentCustomIcon = leaflet.icon({
+  iconUrl: URL_MARKER_CURRENT,
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+});
+
+
 function Map({city,points}:MapScreenProps) {
+
   const mapRef:MutableRefObject<HTMLElement | null> = useRef(null);
   const map = useMap({mapRef, city});
 
-  const defaultCustomIcon = leaflet.icon({
-    iconUrl: URL_MARKER_DEFAULT,
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
-  });
-
-  const currentCustomIcon = leaflet.icon({
-    iconUrl: URL_MARKER_CURRENT,
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
-  });
-
-
   useEffect(()=>{
     if(map) {
+      const markerLayer = layerGroup().addTo(map);
       points.forEach((point) => {
-        leaflet
-          .marker({
+        const marker = new Marker({
             lat: point.location.latitude,
             lng: point.location.longitude,
           })
-          .addTo(map);
+          .addTo(markerLayer);
       });
     }
   },[map,points]);
