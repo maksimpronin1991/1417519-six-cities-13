f65692e32ca9649d7aa4bb6ca24c197b579316dc
Pronin Maks <maksimpronin1991@icloud.com>
2023-07-27 14:36:04 +0300

    module5-task2

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -5,6 +5,8 @@ import Map from '../../components/map/map';
 import { CITY } from '../../mocks/city';
 import { useState } from 'react';
 import HeaderNav from '../../components/header-nav/header-nav';
+import LocationList from '../../components/location-list/location-list';
+import PlacesSortingForm from '../../components/places-sorting-form/places-sorting-form';
 
 type MainScreenProps = {
   rentingOffers: Offers;
@@ -50,38 +52,7 @@ function MainPage ({rentingOffers}: MainScreenProps):JSX.Element {
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
           <section className="locations container">
-            <ul className="locations__list tabs__list">
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Paris</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Cologne</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Brussels</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item  tabs__item--active" >
-                  <span>Amsterdam</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Hamburg</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Dusseldorf</span>
-                </a>
-              </li>
-            </ul>
+            <LocationList/>
           </section>
         </div>
         <div className="cities">
@@ -89,32 +60,7 @@ function MainPage ({rentingOffers}: MainScreenProps):JSX.Element {
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
               <b className="places__found">{rentingOffers.length} places to stay in Amsterdam</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-              Popular
-                  <svg className="places__sorting-arrow" width={7} height={4}>
-                    <use xlinkHref="#icon-arrow-select" />
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li
-                    className="places__option places__option--active"
-                    tabIndex={0}
-                  >
-                Popular
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                Price: low to high
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                Price: high to low
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                Top rated first
-                  </li>
-                </ul>
-              </form>
+              <PlacesSortingForm/>
               <PlacesList
                 rentingOffers = {rentingOffers}
                 onListItemHover={handleListItemHover}
