4f7f972e54712202d093bf87a0154a9adc7919ea
Pronin Maks <maksimpronin1991@icloud.com>
2023-07-27 16:59:09 +0300

    module5-task2

--- src/components/places-sorting-form/places-sorting-form.tsx
+++ src/components/places-sorting-form/places-sorting-form.tsx
@@ -1,15 +1,20 @@
+import { useState } from 'react';
+import cn from 'classnames';
+
 function PlacesSortingForm (){
 
+  const [isOpened, setOpenedState] = useState(false);
+  const opened = isOpened ? 'places__options--opened' : '';
   return (
     <form className="places__sorting" action="#" method="get">
       <span className="places__sorting-caption">Sort by</span>
-      <span className="places__sorting-type" tabIndex={0}>
+      <span className="places__sorting-type" tabIndex={0} onClick={() => setOpenedState(!isOpened)}>
 Popular
         <svg className="places__sorting-arrow" width={7} height={4}>
           <use xlinkHref="#icon-arrow-select" />
         </svg>
       </span>
-      <ul className="places__options places__options--custom places__options--opened">
+      <ul className={cn(`places__options places__options--custom ${opened}`)}>
         <li
           className="places__option places__option--active"
           tabIndex={0}
